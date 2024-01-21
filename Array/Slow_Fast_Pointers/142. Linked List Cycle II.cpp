c2ed3ece60e650ce7f8bf11cfdc552daa3d236f9
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2024-01-21 13:06:28 +0530

    Create 142. Linked List Cycle II.cpp

---
+++ Array/Slow_Fast_Pointers/142. Linked List Cycle II.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* temp = head;
+
+
+        while(fast!=NULL && fast->next != NULL) {
+
+            fast= fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow) {
+                while(temp != slow) {
+                    temp = temp->next;
+                    slow = slow->next;
+                }
+                return temp;
+            }
+        }
+        return NULL;
+    }
+};
