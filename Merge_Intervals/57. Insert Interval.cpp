9230b3e1d5b5ffbb8ec49ca7c29de6e3a5dac9de
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2024-01-27 19:05:40 +0530

    Create 57. Insert Interval.cpp

---
+++ Merge_Intervals/57. Insert Interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
+
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        int n = intervals.size();
+
+        for (int i = 1; i < n; i++) {
+            if (ans.back()[1] >= intervals[i][0]) {
+                if (ans.back()[1] >= intervals[i][1])
+                    continue;
+                ans.back()[1] = intervals[i][1];
+            } else {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
