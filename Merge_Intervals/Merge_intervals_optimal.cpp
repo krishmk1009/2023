7710235ff6df2fdffa352ef3e0f67a06a35413a8
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2024-01-27 18:55:04 +0530

    Create Merge_intervals_optimal.cpp

---
+++ Merge_Intervals/Merge_intervals_optimal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>>ans;
+        sort(intervals.begin() , intervals.end());
+        ans.push_back(intervals[0]);
+
+        for(int i =1 ; i<intervals.size() ; i++){
+
+            if(ans.back()[1] >= intervals[i][0]){
+
+                if( ans.back() [1] >= intervals[i][1]) continue;
+
+                ans.back()[1] = intervals[i][1];
+            }
+
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
