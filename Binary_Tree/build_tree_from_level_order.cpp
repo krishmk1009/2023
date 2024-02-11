fc7815bc0f7596636f35d8cfd7dbeb3159e225d0
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2024-02-11 19:58:18 +0530

    Create build_tree_from_level_order.cpp

---
+++ Binary_Tree/build_tree_from_level_order.cpp
@@ -0,0 +1,123 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class node {
+    
+    public:
+        int data;
+        node* left;
+        node*right;
+        
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+    
+};
+
+    node* buildTree(node* root){
+        
+        
+        int data;
+        cout<<"enter data"<<endl;
+        cin>>data;
+        
+        root = new node(data);
+        if(data ==-1){
+            return NULL;
+        }
+        
+        
+        
+        cout<<"enter data to the left of "<<data<<endl;
+        root->left = buildTree(root->left);
+         cout<<"enter data to the right of "<<data<<endl;
+        root->right = buildTree(root->right);
+        
+        return root;
+    }
+    
+   
+    
+    void levelOrderTraversal(node* root){
+        queue<node*> q;
+        
+        q.push(root);
+        q.push(NULL);
+        
+        while(!q.empty()){
+            
+            node*temp = q.front();
+             q.pop();
+            if(temp == NULL){
+                cout<<endl;
+                
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+                
+            }
+            else{
+              cout<<temp->data<<" ";
+            
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }  
+            }
+            
+        }
+    }
+    
+    void buildLevelOrderTree(node* & root){
+        
+        
+        queue<node*>q;
+        int data;
+        cout<<"enter the data"<<endl;
+        cin>>data;
+        
+        root = new node(data);
+        q.push(root);
+        
+        while(!q.empty()){
+            
+            node*temp = q.front();
+            q.pop();
+            
+            int leftData ;
+            cout<<"enter the data for"<<temp->data<<endl;
+            cin>>leftData;
+            
+            if(leftData !=-1){
+                temp->left = new node(leftData);
+                q.push(temp->left);
+            }
+            
+            int rightData ;
+            cout<<"enter the data for"<<temp->data<<endl;
+            cin>>rightData;
+            
+            if(rightData !=-1){
+                temp->right = new node(rightData);
+                q.push(temp->right);
+            }
+        }
+    }
+
+int main() {
+ 
+    node* root = NULL;
+    
+    buildLevelOrderTree(root);
+    
+    
+    cout<<"printing the level order traversal"<<endl;
+    levelOrderTraversal(root);
+    return 0;
+}
