2fab3f502ea4145f8223b5229ae48c3d56cc2af3
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2024-04-03 23:03:16 +0530

    Create union_types.ts

---
+++ union_types.ts
@@ -0,0 +1,21 @@
+let check: string | number;
+// check = "krusna"
+// check = 2;
+
+type User = {
+  name: string;
+  id: string;
+};
+
+type Admin = {
+  name: number;
+  id: string;
+};
+
+const logginUser: User | Admin = { name: "name", id: "slj" };
+
+// const arr: Array< string | number> = [1, 2, "hello"];
+// or
+const arr: ( string | number)[] = [1, 2, "hello"];
+
+export {};
